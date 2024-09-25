49489d8787dac8f0c827a3065fe33f9735639e74
joaco <joaquin_rodriguez02@hotmail.com.ar>
2024-09-24 23:46:30 -0300

    add completion openIA option.

--- src/openia.ts
+++ src/openia.ts
@@ -1,50 +1,66 @@
 import OpenAI from "openai";
 import "dotenv/config";
-
+import fs from "node:fs";
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
+let promptMessage = null;
+const USING_ASSISTANT = process.env.USING_ASSISTANT === "true";
 export async function convertMsgToQuery(
   userInput: string
 ): Promise<UserQueryType> {
   try {
-    const assistant = await openai.beta.assistants.retrieve(
-      process.env.OPENAI_ASSISTANT_ID
-    );
-    if (!assistant) {
-      console.log("No assistant");
-    }
-    const thread = await openai.beta.threads.create();
-    const message = await openai.beta.threads.messages.create(thread.id, {
-      role: "user",
-      content: userInput,
-    });
-    let run = await openai.beta.threads.runs.createAndPoll(thread.id, {
-      assistant_id: assistant.id,
-    });
-    if (run.status === "completed") {
+    if (USING_ASSISTANT) {
+      const assistant = await openai.beta.assistants.retrieve(
+        process.env.OPENAI_ASSISTANT_ID
+      );
+      if (!assistant) {
+        console.log("No assistant");
+      }
+      const thread = await openai.beta.threads.create();
+      await openai.beta.threads.messages.create(thread.id, {
+        role: "user",
+        content: userInput,
+      });
+      let run = await openai.beta.threads.runs.createAndPoll(thread.id, {
+        assistant_id: assistant.id,
+      });
+      if (run.status !== "completed") {
+        throw new Error("Status not completed");
+      }
       const messages = await openai.beta.threads.messages.list(run.thread_id);
       for (const message of messages.data.reverse()) {
-        if (message.role === "assistant") {
-          const text = message.content[0] as any;
-          console.log("OpenIA response:", text);
-          const assistantResponse: UserQueryType = JSON.parse(text.text.value);
-          if (!assistantResponse) {
-            console.log("Error: ", assistantResponse);
-            return {
-              error: true,
-              query: null,
-              data: null,
-            };
-          }
-
-          console.log(`${message.role} > ${text.text.value}`);
-          return assistantResponse;
+        if (message.role !== "assistant") {
+          return;
         }
+        const text = message.content[0] as any;
+        console.log("OpenIA response:", text);
+        const assistantResponse: UserQueryType = JSON.parse(text.text.value);
+        if (!assistantResponse) {
+          throw new Error("Response could not be parsed");
+        }
+        console.log(`${message.role} > ${text.text.value}`);
+        return assistantResponse;
       }
     } else {
-      console.log(run);
+      if (!promptMessage) {
+        promptMessage = fs.readFileSync("./prompt.json");
+      }
+      const parsedPrompt = JSON.parse(promptMessage);
+      parsedPrompt.push({
+        role: "user",
+        content: userInput,
+      });
+      const completion = await openai.chat.completions.create({
+        model: "gpt-4o",
+        messages: parsedPrompt,
+      });
+      const text = completion.choices[0].message as unknown as string;
+      const completionRes: UserQueryType = JSON.parse(text);
+      if (!completionRes) {
+        throw new Error("Response could not be parsed");
+      }
+      return completionRes;
     }
   } catch (e) {
     console.log(e);
